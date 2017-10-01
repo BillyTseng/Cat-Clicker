0b1afb779a7c05361af312f72cc2ba2b74cb601d
Billy Tseng <tseng.billy@gmail.com>
2017-09-30 21:15:46 -0700

    feat: Add another clicker.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var cat = document.getElementById('cat');
-var clickResult = document.getElementById('clickResult');
-var counter = 1;
-cat.addEventListener('click', function(){
-  console.log(counter);
-  clickResult.innerHTML = '<p id="clickResult">Click counter: '+ counter +'</p>'
-  counter++;
-}, false);
+var elements = document.getElementsByClassName("catimg");
+var catNameArray = ["poplinre", "chewie"];
+
+for (var i = 0; i < elements.length; i++) {
+  var cat = elements[i];
+  var catName = elements[i].previousElementSibling;
+  catName.innerHTML = catNameArray[i];
+  cat.nextElementSibling.value = 0;
+  cat.addEventListener('click', clickCounter, false);
+}
+
+function clickCounter(){
+  var result = this.nextElementSibling;
+  result.value++;
+  result.innerHTML = 'Click counter: '+ result.value;
+}
