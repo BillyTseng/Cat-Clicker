71f0e6701c93a33d16ed5f150564a0ea55082075
Billy Tseng <tseng.billy@gmail.com>
2017-10-01 15:00:52 -0700

    feat: Add a list of cats and an independent image clicker.

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-var elements = document.getElementsByClassName("catimg");
-var catNameArray = ["poplinre", "chewie"];
+var catNameArray = ["poplinre", "chewie", "jetske", "xiaozhen", "yestema"];
+var showcat = document.getElementById("showcat");
+var elem = document.createElement('div');
+var strHtml = '<ul id="namelist">';
 
-for (var i = 0; i < elements.length; i++) {
-  var cat = elements[i];
-  var catName = elements[i].previousElementSibling;
-  catName.innerHTML = catNameArray[i];
-  cat.nextElementSibling.value = 0;
-  cat.addEventListener('click', clickCounter, false);
+for (var i = 0; i < catNameArray.length; i++) {
+  strHtml += '<li>' + catNameArray[i] + '</li>';
+  elem.innerHTML = strHtml;
+};
+strHtml += "</ul>";
+document.body.insertBefore(elem, showcat);
+
+var namelist = document.getElementById("namelist");
+var c = namelist.childNodes;
+for (var i = 0; i < c.length; i++) {
+  c[i].value = 0;
+  c[i].id = i;
+  c[i].addEventListener('click', showCat, false);
 }
 
-function clickCounter(){
-  var result = this.nextElementSibling;
-  result.value++;
-  result.innerHTML = 'Click counter: '+ result.value;
+function showCat() {
+  var strHtml = '';
+  strHtml += '<p id="catname">' + catNameArray[this.id] + '</p>';
+  strHtml += '<img id="cat" class="catimg" src="img/cat' + this.id +'.jpg" width="300px" height="200px">';
+  strHtml += '<p id="clickResult">Click counter: ' + this.value + '</p>';
+  showcat.innerHTML = strHtml;
+
+  var catImage = showcat.childNodes[1];
+  catImage.nextElementSibling.value = 0;
+  catImage.addEventListener('click', clickCounter(this.id), false);
+}
+
+function clickCounter(id) {
+  return function() {
+    var result = c[id];
+    result.value++;
+    this.nextElementSibling.innerHTML = 'Click counter: '+ result.value;
+  }
 }
