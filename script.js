d3f7b7c06ce6f522facf98494b1c77d27fc806d8
Billy Tseng <tseng.billy@gmail.com>
2017-10-02 11:26:46 -0700

    refactor: Apply Model, View, and Octopus(MVO).

--- script.js
+++ script.js
@@ -1,39 +1,109 @@
-var catNameArray = ["poplinre", "chewie", "jetske", "xiaozhen", "yestema"];
-var showcat = document.getElementById("showcat");
-var elem = document.createElement('div');
-var strHtml = '<ul id="namelist">';
-
-for (var i = 0; i < catNameArray.length; i++) {
-  strHtml += '<li>' + catNameArray[i] + '</li>';
-  elem.innerHTML = strHtml;
+var model = {
+  currentCat: null,
+  cats:[
+    {
+      name: 'poplinre',
+      clickCount: 0,
+      imgSrc: 'img/cat0.jpg'
+    },
+    {
+      name: 'chewie',
+      clickCount: 0,
+      imgSrc: 'img/cat1.jpg'
+    },
+    {
+      name: 'jetske',
+      clickCount: 0,
+      imgSrc: 'img/cat2.jpg'
+    },
+    {
+      name: 'xiaozhen',
+      clickCount: 0,
+      imgSrc: 'img/cat3.jpg'
+    },
+    {
+      name: 'yestema',
+      clickCount: 0,
+      imgSrc: 'img/cat4.jpg'
+    }
+  ]
 };
-strHtml += "</ul>";
-document.body.insertBefore(elem, showcat);
 
-var namelist = document.getElementById("namelist");
-var c = namelist.childNodes;
-for (var i = 0; i < c.length; i++) {
-  c[i].value = 0;
-  c[i].id = i;
-  c[i].addEventListener('click', showCat, false);
-}
+var octopus = {
+  init: function() {
+    model.currentCat = model.cats[0];
 
-function showCat() {
-  var strHtml = '';
-  strHtml += '<p id="catname">' + catNameArray[this.id] + '</p>';
-  strHtml += '<img id="cat" class="catimg" src="img/cat' + this.id +'.jpg" width="300px" height="200px">';
-  strHtml += '<p id="clickResult">Click counter: ' + this.value + '</p>';
-  showcat.innerHTML = strHtml;
+    catListView.init();
+    catView.init();
+  },
 
-  var catImage = showcat.childNodes[1];
-  catImage.nextElementSibling.value = 0;
-  catImage.addEventListener('click', clickCounter(this.id), false);
-}
+  getCurrentCat: function() {
+      return model.currentCat;
+  },
 
-function clickCounter(id) {
-  return function() {
-    var result = c[id];
-    result.value++;
-    this.nextElementSibling.innerHTML = 'Click counter: '+ result.value;
+  setCurrentCat: function(cat) {
+      model.currentCat = cat;
+  },
+
+  getCats: function() {
+      return model.cats;
+  },
+
+  clickCounter: function() {
+    model.currentCat.clickCount++;
+    catView.render();
   }
-}
+};
+
+var catView = {
+  init: function() {
+    var showcat = document.getElementById("showcat");
+    this.render();
+  },
+
+  render: function() {
+    var cat = octopus.getCurrentCat();
+    var strHtml = '';
+    strHtml += '<p id="catname">' + cat.name + '</p>';
+    strHtml += '<img id="cat" class="catimg" src=' + cat.imgSrc +' width="300px" height="200px">';
+    strHtml += '<p id="clickResult">Click counter: ' + cat.clickCount + '</p>';
+    showcat.innerHTML = strHtml;
+    var catImage = showcat.childNodes[1];
+    catImage.addEventListener('click', function() {
+      octopus.clickCounter();
+    }, false);
+  }
+};
+
+var catListView = {
+  init: function() {
+    this.render();
+  },
+
+  render: function() {
+    var elem = document.createElement('div');
+    var showcat = document.getElementById("showcat");
+    var cats = octopus.getCats();
+    var strHtml = '<ul id="namelist">';
+
+    for (var i = 0; i < cats.length; i++) {
+      strHtml += '<li>' + cats[i].name + '</li>';
+      elem.innerHTML = strHtml;
+    };
+    strHtml += "</ul>";
+    document.body.insertBefore(elem, showcat);
+    var namelist = document.getElementById("namelist");
+    var c = namelist.childNodes;
+    for (var i = 0; i < c.length; i++) {
+      var cat = cats[i];
+      c[i].addEventListener('click', function(catCopy) {
+        return function() {
+          octopus.setCurrentCat(catCopy);
+          catView.render();
+        }
+      }(cat), false);
+    }
+  }
+};
+
+octopus.init();
