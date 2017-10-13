7e0ae6f2fe99c2dfc101facd30177a4df5afed5a
Billy Tseng <tseng.billy@gmail.com>
2017-10-13 12:44:56 -0700

    feat: List all cat's names and switch the view while clicking the name.

--- knockout/js/app.js
+++ knockout/js/app.js
@@ -1,3 +1,36 @@
+var initialCats = [
+  {
+    name: 'poplinre',
+    clickCount: 0,
+    imgSrc: 'img/cat0.jpg',
+    nickname: [{ name: 'pop' }, { name: 'poppy' }]
+  },
+  {
+    name: 'chewie',
+    clickCount: 0,
+    imgSrc: 'img/cat1.jpg',
+    nickname: [{ name: 'chew' }]
+  },
+  {
+    name: 'jetske',
+    clickCount: 0,
+    imgSrc: 'img/cat2.jpg',
+    nickname: [{ name: 'cake' }, { name: 'airplane' }]
+  },
+  {
+    name: 'xiaozhen',
+    clickCount: 0,
+    imgSrc: 'img/cat3.jpg',
+    nickname: [{ name: 'zhen' }, { name: 'chen' }]
+  },
+  {
+    name: 'yestema',
+    clickCount: 0,
+    imgSrc: 'img/cat4.jpg',
+    nickname: [{ name: 'yesman' }]
+  }
+];
+
 function cat(data) {
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -16,18 +49,18 @@ function cat(data) {
 }
 
 var ViewModel = function() {
-  this.currentCat = ko.observable(new cat({
-    clickCount: 0,
-    name: 'Kitty',
-    imgSrc: 'img/22252709_010df3379e_z.jpg',
-    nickname: [
-      { name: 'Bert' },
-      { name: 'Charles' },
-      { name: 'Denise' }
-    ]
-  }));
+  var self = this;
+  this.catList = ko.observableArray([]);
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new cat(catItem));
+  });
+  this.currentCat = ko.observable(this.catList()[0]);
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
+  };
+
+  this.catSwitcher = function(clickedCat) {
+    self.currentCat(clickedCat);
   };
 }
 
